38a2628e14644d5c5e2bdc4970c48fc7fea071a7
Pejh <pejhmon@users.noreply.github.com>
2015-08-12 17:28:01 +0100

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -7,11 +7,14 @@
  * @author pejhmon
  */
 
+// Server details
 $serverName = "appetite";
 $dbname = 'testdb1';
-$dbid = "app@".$serverName;
-$dbpwd = iconv('','UTF-8','Admin12£');
+$serverAdmin = "app";
+$serverPassword = "Admin12£"
 
+$dbpwd = iconv('','UTF-8',$serverPassword);
+$dbid = $serverAdmin."@".$serverName;
 $server = "tcp:".$serverName.".database.windows.net,1433";
 $connectionOptions = array( "Database"=>$dbname, "UID"=>$dbid, "PWD"=>$dbpwd);
 $conn = sqlsrv_connect($server, $connectionOptions);
