f3d82585f57a2a08d95c754d7c6ee3237b38b7ae
Pejh <pejhmon@users.noreply.github.com>
2015-08-13 11:19:13 +0100

    Update home.php

--- home.php
+++ home.php
@@ -10,6 +10,15 @@ echo $json['Server']['Name'];
 echo $json['Server'][Admin];
 echo $json[Server][Password];
 echo "Complete Dickhead";
+
+foreach ($jsonIterator as $key => $val) {
+    if(is_array($val)) {
+        echo "$key:\n";
+    } else {
+        echo "$key => $val\n";
+    }
+}
+echo "Dickhead number 2";
 ?>
 
 <html>
