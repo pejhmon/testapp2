9189504259a91a24ce010b2a3eecec426df4b366
Pejh <pejhmon@users.noreply.github.com>
2015-08-13 11:10:12 +0100

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -6,12 +6,7 @@
  * @version 1.0
  * @author pejhmon
  */
-// REST import
-$jsonURL = file_get_contents("nhs-json.azurewebsites.net/rest.json");
-$json = json_decode($jsonURL, true);
-echo $json['ServerName'];
-echo $json['ServerAdmin'];
-echo $json['ServerPassword'];
+
 // Server details
 $serverName = "appetite";
 $dbname = "testdb1";
