d80618f74e26b4a0846c5bcb4a8e5ffe7b7f2ff5
Pejh <pejhmon@users.noreply.github.com>
2015-08-14 10:50:39 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,34 +1,6 @@
 <?php
 require_once 'dbconnection.php'; 
 ?>
-
-<?php
-// REST import
-$RESTstring = file_get_contents('http://nhs-json.azurewebsites.net');
-
-//$json = json_decode($jsonURL, true);
-//echo $json['Server']['Name'];
-//echo $json['Server'][Admin];
-//echo $json[Server][Password];
-//echo "Complete Dickhead";
-//echo $jsonURL;
-//echo $json;
-
-$RESTarray = explode(':::',$RESTstring);
-//echo $RESTarray[0];
-//echo '<br/>';
-echo $RESTarray[1];
-echo '<br/>';
-//echo $RESTarray[2];
-//echo '<br/>';
-echo $RESTarray[3];
-echo '<br/>';
-//echo $RESTarray[4];
-//echo '<br/>';
-echo $RESTarray[5];
-
-?>
-
 <html>
     <head>
         <title>Home Page</title>
